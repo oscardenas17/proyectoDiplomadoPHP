48f07f0b0277e814c03ecdd5b04a15051c87cfde
oscardenas17 <oscardenas17@gmail.com>
2020-01-11 19:22:04 -0500

    Form login

--- index.php
+++ index.php
@@ -7,6 +7,20 @@
     <title>Diplomado - sistema de notas</title>
 </head>
 <body>
-    
+
+
+    <h1>Inicio de Sesión</h1>    
+    <form method="POST" action="Usuarios/Controller/login.php">
+
+        <label for="Usuario">Usuario: </label>  <br> 
+        <input type="text" name="Usuario" required="" autocomplete="off" placeholder="Ingresa tu usuario"> <br>
+
+        <label for="Contrasena">Contraseña: </label><br> 
+        <input type="password" name="Contrasena" required="" autocomplete="off" placeholder="Ingresa tu contraseña" > <br> 
+
+        <input type="submit" value="Iniciar sesion" >
+
+
+    </form>
 </body>
 </html>
